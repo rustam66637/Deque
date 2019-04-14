8e0183e2509354b94bd745ee8f09ba33f92c6210
Rustam <43505186+rustam66637@users.noreply.github.com>
2019-04-14 21:51:34 +0500

    Create Deque.py

---
+++ Deque.py
@@ -0,0 +1,20 @@
+class Deque:
+    def __init__(self):
+        # инициализация внутреннего хранилища
+
+    def addFront(self, item):
+        # добавление в голову
+
+    def addTail(self, item):
+        # добавление в хвост
+
+    def removeFront(self):
+        # удаление из головы
+        return None # если очередь пуста
+
+    def removeTail(self):
+        # удаление из хвоста
+        return None # если очередь пуста
+
+    def size(self):
+        return 0 # размер очереди
